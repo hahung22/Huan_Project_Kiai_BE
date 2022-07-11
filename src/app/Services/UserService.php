e55a4b38c8fce7d44843455ce58e2c87b2efb1d4
Huan Nguyen <nguyentranghuan638@gmail.com>
2022-07-11 13:54:36 +0700

    api user login, logout, register

--- src/app/Services/UserService.php
+++ src/app/Services/UserService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Auth;
 
 class UserService
 {
@@ -11,5 +12,31 @@ class UserService
     public function __construct(UserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
+    }
+
+    public function registerUser($data)
+    {
+        $data = array_merge($data, ['password' => bcrypt($data['password'])]);
+        return $this->userRepository->register($data);
+    }
+
+    public function getUserVerify($id)
+    {
+        return $this->userRepository->getUserVerify($id);
+    }
+
+    public function loginUser($data)
+    {
+        return $this->userRepository->login($data);
+    }
+
+    public function getUser()
+    {
+        return Auth::user();
+    }
+
+    public function userLogout()
+    {
+        return Auth::logout();
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +14,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($route) {
+    Route::post('/register', [AuthController::class, 'register'])->name('user.register');
+    Route::get('verify-email/{id}/{hash}', [AuthController::class, 'verify'])->name('verification.verify');
+    Route::post('/login', [AuthController::class, 'login'])->name('user.login');
+    Route::get('/profile', [AuthController::class, 'profile'])->name('user.profile');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('user.logout');
+});
